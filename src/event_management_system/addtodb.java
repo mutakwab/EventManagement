6af3614f9244ec8ca89be4f5530af40017479f51
remy20002021 <ishiremyaime@gmail.com>
2023-12-13 21:32:57 +0200

    changes

--- src/event_management_system/addtodb.java
+++ src/event_management_system/addtodb.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package event_management_system;
 
 import java.awt.Color;
@@ -137,8 +133,8 @@ public class addtodb extends JFrame{
         setVisible(true);
         }
      public static void main(String args[]){
-//         String s[] = new String[]{"id","name","image_url","price","capacity","facilites","description"};
-//         addtodb a = new addtodb(s);
+       String s[] = new String[]{"id","name","image_url","price","capacity","facilites","description"};
+        addtodb a = new addtodb(s);
      }
 
     
